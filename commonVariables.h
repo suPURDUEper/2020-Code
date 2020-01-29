0ec6ae598f4adaad77eabf95902b7d27fcd1042e
AudreyWilliamson <40180816+AudreyWilliamson@users.noreply.github.com>
2020-01-29 18:53:26 -0500

    Update commonVariables.h

--- commonVariables.h
+++ commonVariables.h
@@ -1,40 +1,43 @@
 #pragma once
+#ifndef commonVariables
+#define commonVariables
+
 #include <frc/DoubleSolenoid.h>
 #include <frc/Victor.h>
 #include <rev/SparkMax.h>
 #include <ctre/Phoenix.h>
+#include <rev/CANSparkMax.h>
 
 using namespace frc;
 using namespace rev;
-
+using namespace ctre;
+using namespace std;
 
 //      XBAY Button Defines     //    
 extern bool btnA;
 extern bool btnB;
 extern bool btnX;
 extern bool btnY;
+
 //      XY Axes Defines      //
-extern int leftAxisX;
-extern int leftAxisY;
-extern int rightAxisX;
-extern int rightAxisY;
+extern double leftAxisX;
+extern double leftAxisY;
+extern double rightAxisX;
+extern double rightAxisY;
+
 //      Bumper and Trigger Defines        //
 extern bool leftBumper;
 extern bool rightBumper;
 extern bool leftTrigger;
 extern bool rightTrigger;
+
 //      Misc. Button Defines      //
 extern bool btnBack;
 extern bool btnStart;
 extern bool leftJoystickClick;
 extern bool rightJoystickClick;
-//      flightStickDefines      //
+
+//      Flight Stick Defines      //
 extern int hat;
-//      Solenoids       //
-frc::DoubleSolenoid hoodSolenoid;
-//      Motors      //
-Victor sliderMotor1;
-Victor sliderMotor2;
-SparkMax flyWheelL(1);
-SparkMax flyWheelR(2);
-WPI_TalonSRX controlPanelMotor(3);
+
+#endif
