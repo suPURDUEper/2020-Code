15a31005dbb1c4a8dabd433e6e2ee09d86a3ecf6
AudreyWilliamson <40180816+AudreyWilliamson@users.noreply.github.com>
2020-01-29 20:18:19 -0500

    Create Auto.h

---
+++ Auto.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <string>
+
+#include <frc/TimedRobot.h>
+#include <frc/smartdashboard/SendableChooser.h>
+#include <frc/Joystick.h>
+#include <frc/drive/DifferentialDrive.h>
+
+
+#include <rev/CANSparkMax.h>
+#include <ctre/Phoenix.h>
+
+#ifndef Auto_H_
+#define Auto_H_
+
+extern int timer;
+
+void Auto1();
+void Auto2();
+void Auto3();
+
+#endif
