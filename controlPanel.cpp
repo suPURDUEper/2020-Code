8e4e3eee509add66982d222a29af2b14a5dacbc7
AudreyWilliamson <40180816+AudreyWilliamson@users.noreply.github.com>
2020-01-29 20:12:32 -0500

    Update controlPanel.cpp

--- controlPanel.cpp
+++ controlPanel.cpp
@@ -1,15 +1,15 @@
+  
 #include "commonVariables.h"
-#include <ctre/phoenix/motorcontrol/can/TalonSRX.h>
+#include "motorDefine.h"
 
-
+WPI_TalonSRX controlPanelMotor(3);
 
 void controlPanel() {
-    if (hat = 90) {
+    if ((hat = 90)) {
         controlPanelMotor.Set(0.5);
-    } else if (hat = 270) {
+    } else if ((hat = 270)) {
         controlPanelMotor.Set(-0.5);
     } else {
         controlPanelMotor.Set(0);
     }
 }
-
