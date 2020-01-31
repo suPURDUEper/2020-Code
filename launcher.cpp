8bb8050f6e672dbe00eab1553c737b01b8bdefcd
JesseRang <47062808+JesseRang@users.noreply.github.com>
2020-01-30 19:40:50 -0500

    Update launcher.cpp

--- launcher.cpp
+++ launcher.cpp
@@ -1,20 +1,40 @@
+#include <ctre/Phoenix.h>
+#include <iostream>
+#include <rev/SparkMax.h>
 #include "commonVariables.h"
-#include "motorDefine.h"
-
+/*
 SparkMax flyWheelL(1);
 SparkMax flyWheelR(2);
 
 void launcher() {
-    if (btnX) {
-        if (flyWheelL.GetSpeed() < .8 || flyWheelR.GetSpeed() < .8) {
-            flyWheelL.Set(1);
-            flyWheelR.Set(1);
-        } else if (flyWheelL.GetSpeed() > .8 || flyWheelR.GetSpeed() > .8) {
-            flyWheelL.Set(.6);
-            flyWheelR.Set(.6);
-        } else {
-            flyWheelL.Set(0);
-            flyWheelR.Set(0);
+    //if (rightTrigger) {
+        if (!btnX/*flyWheelL.GetSpeed() < 0.8 && flyWheelR.GetSpeed() < 0.8*//* ) {
+            std::cout << "0";// flyWheelL.Set(1);
+            //flyWheelR.Set(1);
+        } else if (btnX/*flyWheelL.GetSpeed() > 0.8 && flyWheelR.GetSpeed() > 0.8*///) {
+            //std::cout << "1";// flyWheelL.Set(0.6);
+            //flyWheelR.Set(0.6);
+        //} 
+    //} else if (!leftTrigger) {
+        //flyWheelL.Set(0);
+        //flyWheelR.Set(0);
+    //}
+
+//}
+
+/*
+    Comment left by Zach:
+    I recommend using a while loop rather than an if statement, fore example.
+
+    while(rightTrigger)
+        {
+            limelight will get the distance to the target;
+            calculate the target rpm;
+
+            while(velocity != rpm)
+                {
+                    check the target rpm
+                }
+            shoot ball
         }
-    }
-}
+*/
