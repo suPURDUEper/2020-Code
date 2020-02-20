af9046244f6b522aa4e85c3b3bb2c076a5b19128
AudreyWilliamson <40180816+AudreyWilliamson@users.noreply.github.com>
2020-02-20 17:00:59 -0500

    Add files via upload

---
+++ NewMainCode/src/main/cpp/conveyor.cpp
@@ -0,0 +1,24 @@
+#include "commonVariables.h"
+/*
+    Description: A function to detect when balls are coming in and out
+    Pre: No balls in the robot
+    Post: Balls coming in and out while the robot is running, but never exceeding 5 balls total 
+*/
+double conveyor()
+{
+    double ConveyorSpeed = 0;
+   /* if ((breakBeamBottom || !breakBeamTop) && !rightTrigger0) // if bottom beam is broken and top beam not, then increase ball count by 1
+    {
+        ConveyorSpeed = 0;
+    }
+    else if (!breakBeamBottom && breakBeamTop) //took out not triggered b/c robot needs to run coneyor and shoot at the same time
+    {
+        ConveyorSpeed = 0.5;
+    }*/
+    if (btnB1)
+    {
+        ConveyorSpeed = -0.5;
+    }
+
+    return ConveyorSpeed;
+}
---
+++ NewMainCode/src/main/cpp/driveMath.h
@@ -0,0 +1,33 @@
+#ifndef DRIVEMATH_H_
+#define DRIVEMATH_H_
+
+#include <math.h>
+
+double straightMath(float driveMinPower, float modValue, double leftAxisY0)
+{
+    double driveStraight;
+    double maxDriveStraight = 0.85;
+    if (leftAxisY0 > 0.05) // Take the Drive Input and delinearize it for the Left Axis
+        driveStraight = (driveMinPower + (maxDriveStraight - driveMinPower) * (modValue * (pow(leftAxisY0, 3) + (1 - modValue) * leftAxisY0)));
+    else if (leftAxisY0 < -0.05)
+        driveStraight = ((-1 * driveMinPower) + (maxDriveStraight - driveMinPower) * (modValue * (pow(leftAxisY0, 3) + (1 - modValue) * leftAxisY0)));
+    else
+        driveStraight = 0;
+
+    return driveStraight;
+}
+
+double turnMath(float turnMinPower, float modValueTurn, double rightAxisX0)
+{
+    double driveTurn;
+    double maxDriveTurn = 0.85;
+    if (rightAxisX0 > 0.05) // Take the Drive Input and delinearize it for the Right Axis
+        driveTurn = (turnMinPower + (maxDriveTurn - turnMinPower) * (modValueTurn * (pow(rightAxisX0, 3) + (1 - modValueTurn) * rightAxisX0)));
+    else if (rightAxisX0 < -0.05)
+        driveTurn = ((-1 * turnMinPower) + (maxDriveTurn - turnMinPower) * (modValueTurn * (pow(rightAxisX0, 3) + (1 - modValueTurn) * rightAxisX0)));
+    else
+        driveTurn = 0;
+
+    return driveTurn;
+}
+#endif
